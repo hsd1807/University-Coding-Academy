6d9643d156442a54656357c9aada83690bb29cd2
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-08-08 09:51:53 +0530

    Completed

---
+++ Advance Programming in C/Bits & Bytes/bitAnd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int bitAnd(int x, int y) {
+	return ~(~x | ~y);
+}
+
+int main() {
+	int x = 13
+  int y = 18;
+	printf("%d", x & y);
+	printf("\n%d", bitAnd(x, y));
+	return 0;
+}
