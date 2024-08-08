af1d40b5ebb89ff0c8e4af3ec089b4c3303601be
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-08-08 10:15:18 +0530

    Completed

--- Advance Programming in C/Bits & Bytes/bitAnd.c
+++ Advance Programming in C/Bits & Bytes/bitAnd.c
@@ -7,7 +7,7 @@ int bitAnd(int x, int y) {
 int main() {
 	int x = 13
   	int y = 18;
-	printf("%d", x & y);
-	printf("\n%d", bitAnd(x, y));
+	printf("%d\n", x & y);
+	printf("%d\n", bitAnd(x, y));
 	return 0;
 }
